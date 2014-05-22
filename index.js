8c97352335f776a1a4b86ea652af692f323e5542
Brian Vallelunga <vallelungabrian@gmail.com>
2014-05-22 10:52:56 +0000

    tweaked css for borderra-radius and etc

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* Compiled by kdc on Thu May 22 2014 09:09:23 GMT+0000 (UTC) */
+/* Compiled by kdc on Thu May 22 2014 10:52:14 GMT+0000 (UTC) */
 (function() {
 /* KDAPP STARTS */
 /* BLOCK STARTS: /home/bvallelunga/Applications/Koders.kdapp/index.coffee */
